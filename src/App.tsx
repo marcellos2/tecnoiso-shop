33eb298412184aafae599d6463c2048aa47d08a0
marcellos2 <mclsouza1613ad@gmail.com>
2026-01-28 13:40:57 -0300

    correções rotas

--- src/App.tsx
+++ src/App.tsx
@@ -5,37 +5,36 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartProvider } from "@/contexts/CartContext";
 import Index from "./pages/Index";
+import Auth from "./pages/Auth";
+import AuthCallback from "./pages/Authcallback";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
-import OrderConfirmed from "./pages/OrderConfirmed";
 import ProductDetail from "./pages/ProductDetail";
-import Auth from "./pages/Auth";
-import AuthCallback from "./pages/Authcallback"; // NOVA IMPORTAÇÃO
+import OrderConfirmed from "./pages/OrderConfirmed";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <CartProvider>
-      <TooltipProvider>
+    <TooltipProvider>
+      <CartProvider>
         <Toaster />
         <Sonner />
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/auth" element={<Auth />} />
-            <Route path="/auth/callback" element={<AuthCallback />} /> {/* NOVA ROTA */}
-            <Route path="/carrinho" element={<Cart />} />
+            <Route path="/auth/callback" element={<AuthCallback />} />
+            <Route path="/cart" element={<Cart />} />
             <Route path="/checkout" element={<Checkout />} />
-            <Route path="/pedido-confirmado" element={<OrderConfirmed />} />
             <Route path="/produto/:id" element={<ProductDetail />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="/order-confirmed" element={<OrderConfirmed />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
-      </TooltipProvider>
-    </CartProvider>
+      </CartProvider>
+    </TooltipProvider>
   </QueryClientProvider>
 );
 
